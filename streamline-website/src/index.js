720e2be59ec5a353baa23c5b73faedbadc6262b8
Vasilios Kambouras <Vasilioskw@gmail.com>
2024-07-08 12:46:35 -0400

    try again

--- streamline-website/src/index.js
+++ streamline-website/src/index.js
@@ -5,15 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
